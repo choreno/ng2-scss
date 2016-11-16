d47d0d1386a9eb7e48984b371eadd8fbf9b1303c
THBar <choreno@gmail.com>
2016-11-16 15:43:01 -0500

    removed angular material and added bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+// import { MaterialModule } from '@angular/material';
+
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    // MaterialModule.forRoot(),
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
